c342c900ae148dfc3236525ad717db68cbb445aa
aimok04 <aimok04@mailbox.org>
2024-08-16 18:34:27 +0200

    release: 1.0.0-alpha.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "de.kitshn.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 101
-        versionName = "1.0.0-alpha.2"
+        versionCode = 102
+        versionName = "1.0.0-alpha.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
