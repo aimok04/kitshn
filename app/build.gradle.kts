5439798709b1eddcdfd6a6039fd9acf0d8678e12
aimok04 <aimok04@mailbox.org>
2024-08-19 01:05:22 +0200

    fix(build.gradle): added minifying back

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,14 @@ android {
         debug {
             applicationIdSuffix = ".debug"
         }
-        release { }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
