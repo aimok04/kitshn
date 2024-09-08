cc0356ba2734430d7e7146369f5482eccebafeda
aimok04 <aimok04@mailbox.org>
2024-09-08 17:32:24 +0200

    release: 1.0.0-alpha.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "de.kitshn.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 104
-        versionName = "1.0.0-alpha.5"
+        versionCode = 105
+        versionName = "1.0.0-alpha.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
