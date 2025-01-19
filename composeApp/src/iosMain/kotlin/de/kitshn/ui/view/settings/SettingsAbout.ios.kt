ca57c5fb6fc3e329e2cfb6dafa759760c53e8128
aimok04 <aimok04@mailbox.org>
2025-01-19 21:24:20 +0100

    feat(iosMain/settings): added "Rate in App Store" button to SettingsAbout

--- composeApp/src/iosMain/kotlin/de/kitshn/ui/view/settings/SettingsAbout.ios.kt
+++ composeApp/src/iosMain/kotlin/de/kitshn/ui/view/settings/SettingsAbout.ios.kt
@@ -1,5 +1,28 @@
 package de.kitshn.ui.view.settings
 
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.RateReview
+import androidx.compose.material3.Text
+import de.kitshn.launchMarketPageHandler
+import de.kitshn.ui.component.settings.SettingsListItem
+import kitshn.composeapp.generated.resources.Res
+import kitshn.composeapp.generated.resources.common_review
+import kitshn.composeapp.generated.resources.settings_section_about_item_review_ios_description
+import org.jetbrains.compose.resources.stringResource
 
-actual fun LazyListScope.platformSpecificItems() {}
+actual fun LazyListScope.platformSpecificItems() {
+    item {
+        val launchMarketPlace = launchMarketPageHandler()
+
+        SettingsListItem(
+            label = { Text(stringResource(Res.string.common_review)) },
+            description = { Text(stringResource(Res.string.settings_section_about_item_review_ios_description)) },
+            icon = Icons.Rounded.RateReview,
+            contentDescription = stringResource(Res.string.common_review),
+            onClick = {
+                launchMarketPlace()
+            }
+        )
+    }
+}
