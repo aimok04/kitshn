7efe144d860d48113160dba8e5c7ae960f470c86
aimok04 <aimok04@mailbox.org>
2025-03-08 00:47:40 +0100

    feat(commonMain/recipeBooks): show placeholder icon when book is empty to avoid endless loading animation

--- composeApp/src/commonMain/kotlin/de/kitshn/ui/component/model/recipebook/HorizontalRecipeBookCard.kt
+++ composeApp/src/commonMain/kotlin/de/kitshn/ui/component/model/recipebook/HorizontalRecipeBookCard.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Receipt
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -19,7 +21,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +46,7 @@ import de.kitshn.ui.state.ErrorLoadingSuccessState
 import de.kitshn.ui.theme.Typography
 import de.kitshn.ui.theme.playfairDisplay
 import kitshn.composeapp.generated.resources.Res
+import kitshn.composeapp.generated.resources.common_cook_book
 import kitshn.composeapp.generated.resources.common_favorites
 import kitshn.composeapp.generated.resources.common_loading
 import kitshn.composeapp.generated.resources.recipe_book_favorites_description
@@ -71,6 +77,8 @@ fun HorizontalRecipeBookCard(
         defaultCardColors = CardDefaults.elevatedCardColors()
     )
 
+    var showPlaceholderIcon by remember { mutableStateOf(false) }
+
     val listEntriesState = rememberTandoorRequestState()
     LaunchedEffect(recipeBook) {
         if(recipeBook?.entries?.size == 0) listEntriesState.wrapRequest {
@@ -78,6 +86,8 @@ fun HorizontalRecipeBookCard(
             if(recipeBook.entries.isEmpty()
                 && recipeBook.filter != null
             ) recipeBook.listFilterEntries(1)
+
+            showPlaceholderIcon = recipeBook.entries.isEmpty() && recipeBook.filterEntries.isEmpty()
         }
     }
 
@@ -131,22 +141,36 @@ fun HorizontalRecipeBookCard(
                             )
                         }
                     } else {
-                        AsyncImage(
-                            model = recipeBook?.loadThumbnail(),
-                            contentDescription = recipeBook?.name,
-                            contentScale = ContentScale.Crop,
-                            imageLoader = imageLoader,
-                            modifier = Modifier
-                                .height(42.dp)
-                                .width(42.dp)
-                                .clip(RoundedCornerShape(8.dp))
-                                .loadingPlaceHolder(
-                                    ErrorLoadingSuccessState.bool(
-                                        recipeBook?.entries?.isNotEmpty() == true
-                                                || recipeBook?.filterEntries?.isNotEmpty() == true
-                                    )
+                        if(showPlaceholderIcon) {
+                            Box(
+                                Modifier.size(42.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                                    .background(MaterialTheme.colorScheme.primaryContainer),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Receipt,
+                                    contentDescription = stringResource(Res.string.common_cook_book)
                                 )
-                        )
+                            }
+                        } else {
+                            AsyncImage(
+                                model = recipeBook?.loadThumbnail(),
+                                contentDescription = recipeBook?.name,
+                                contentScale = ContentScale.Crop,
+                                imageLoader = imageLoader,
+                                modifier = Modifier
+                                    .height(42.dp)
+                                    .width(42.dp)
+                                    .clip(RoundedCornerShape(8.dp))
+                                    .loadingPlaceHolder(
+                                        ErrorLoadingSuccessState.bool(
+                                            recipeBook?.entries?.isNotEmpty() == true
+                                                    || recipeBook?.filterEntries?.isNotEmpty() == true
+                                        )
+                                    )
+                            )
+                        }
                     }
                 }
             },
