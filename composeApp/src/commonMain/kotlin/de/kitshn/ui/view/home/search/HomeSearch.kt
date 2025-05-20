1185c7b206ec3c49dcb2730f8814bee7f264b08f
aimok04 <aimok04@mailbox.org>
2025-05-20 15:51:33 +0200

    feat(commonMain/search): improved buttons and cursor position when reopening dialog (resolved #203)
    Signed-off-by: aimok04 <aimok04@mailbox.org>

--- composeApp/src/commonMain/kotlin/de/kitshn/ui/view/home/search/HomeSearch.kt
+++ composeApp/src/commonMain/kotlin/de/kitshn/ui/view/home/search/HomeSearch.kt
@@ -1,17 +1,30 @@
 package de.kitshn.ui.view.home.search
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SearchBarDefaults.InputFieldHeight
+import androidx.compose.material3.SearchBarDefaults.inputFieldColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,7 +33,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import co.touchlab.kermit.Logger
 import de.kitshn.BackHandler
 import de.kitshn.KitshnViewModel
@@ -28,8 +54,8 @@ import de.kitshn.api.tandoor.model.recipe.TandoorRecipeOverview
 import de.kitshn.ui.selectionMode.model.RecipeSelectionModeTopAppBar
 import de.kitshn.ui.selectionMode.rememberSelectionModeState
 import kitshn.composeapp.generated.resources.Res
+import kitshn.composeapp.generated.resources.action_back
 import kitshn.composeapp.generated.resources.action_close
-import kitshn.composeapp.generated.resources.common_search
 import kitshn.composeapp.generated.resources.home_search_tandoor
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.stringResource
@@ -55,15 +81,23 @@ fun ViewHomeSearch(
         state.openWithKeywordId(client, it)
     }
 
-    var query by rememberSaveable { mutableStateOf(state.query) }
+    var query by rememberSaveable {
+        mutableStateOf(
+            TextFieldValue(
+                text = state.query,
+                selection = TextRange(state.query.length)
+            )
+        )
+    }
+
     LaunchedEffect(query) {
         delay(200)
-        state.query = query
+        state.query = query.text
     }
 
     if(state.shown.value) SearchBar(
         inputField = {
-            SearchBarDefaults.InputField(
+            SearchInputField(
                 modifier = Modifier.focusRequester(focusRequester),
                 query = query,
                 onQueryChange = {
@@ -71,20 +105,25 @@ fun ViewHomeSearch(
                 },
                 onSearch = {
                     keyboardController?.hide()
-                    state.query = it
+                    state.query = it.text
                 },
                 expanded = true,
                 onExpandedChange = { },
                 placeholder = { Text(stringResource(Res.string.home_search_tandoor)) },
                 leadingIcon = {
-                    Icon(
-                        Icons.Rounded.Search,
-                        contentDescription = stringResource(Res.string.common_search)
-                    )
+                    IconButton(onClick = {
+                        state.dismiss()
+                    }) {
+                        Icon(
+                            Icons.AutoMirrored.Rounded.ArrowBack,
+                            contentDescription = stringResource(Res.string.action_back)
+                        )
+                    }
                 },
                 trailingIcon = {
                     IconButton(onClick = {
-                        state.dismiss()
+                        query = TextFieldValue("")
+                        focusRequester.requestFocus()
                     }) {
                         Icon(
                             Icons.Rounded.Close,
@@ -130,3 +169,114 @@ fun ViewHomeSearch(
         }
     }
 }
+
+
+/**
+ * Copy of SearchBarDefaults.InputField, replacing "String" with "TextFieldValue" to allow setting "selection" value.
+ */
+@ExperimentalMaterial3Api
+@Composable
+fun SearchInputField(
+    query: TextFieldValue,
+    onQueryChange: (TextFieldValue) -> Unit,
+    onSearch: (TextFieldValue) -> Unit,
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    colors: TextFieldColors = inputFieldColors(),
+    interactionSource: MutableInteractionSource? = null,
+) {
+    @Suppress("NAME_SHADOWING")
+    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+
+    val focused = interactionSource.collectIsFocusedAsState().value
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    val textColor =
+        LocalTextStyle.current.color.takeOrElse {
+            colors.textColor(enabled, isError = false, focused = focused)
+        }
+
+    BasicTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        modifier =
+            modifier
+                .sizeIn(
+                    minWidth = 360.dp,
+                    maxWidth = 720.dp,
+                    minHeight = InputFieldHeight,
+                )
+                .focusRequester(focusRequester)
+                .onFocusChanged { if (it.isFocused) onExpandedChange(true) }
+                .semantics {
+                    onClick {
+                        focusRequester.requestFocus()
+                        true
+                    }
+                },
+        enabled = enabled,
+        singleLine = true,
+        textStyle = LocalTextStyle.current.merge(TextStyle(color = textColor)),
+        cursorBrush = SolidColor(colors.cursorColor(isError = false)),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = { onSearch(query) }),
+        interactionSource = interactionSource,
+        decorationBox =
+            @Composable { innerTextField ->
+                TextFieldDefaults.DecorationBox(
+                    value = query.text,
+                    innerTextField = innerTextField,
+                    enabled = enabled,
+                    singleLine = true,
+                    visualTransformation = VisualTransformation.None,
+                    interactionSource = interactionSource,
+                    placeholder = placeholder,
+                    leadingIcon =
+                        leadingIcon?.let { leading ->
+                            { Box(Modifier.offset(x = 4.dp)) { leading() } }
+                        },
+                    trailingIcon =
+                        trailingIcon?.let { trailing ->
+                            { Box(Modifier.offset(x = (-4).dp)) { trailing() } }
+                        },
+                    shape = SearchBarDefaults.inputFieldShape,
+                    colors = colors,
+                    contentPadding = TextFieldDefaults.contentPaddingWithoutLabel(),
+                    container = {},
+                )
+            }
+    )
+
+    val shouldClearFocus = !expanded && focused
+    LaunchedEffect(expanded) {
+        if (shouldClearFocus) {
+            // Not strictly needed according to the motion spec, but since the animation
+            // already has a delay, this works around b/261632544.
+            delay(100.0.toLong())
+            focusManager.clearFocus()
+        }
+    }
+}
+
+@Stable
+internal fun TextFieldColors.textColor(
+    enabled: Boolean,
+    isError: Boolean,
+    focused: Boolean,
+): Color =
+    when {
+        !enabled -> disabledTextColor
+        isError -> errorTextColor
+        focused -> focusedTextColor
+        else -> unfocusedTextColor
+    }
+
+@Stable
+internal fun TextFieldColors.cursorColor(isError: Boolean): Color =
+    if (isError) errorCursorColor else cursorColor
