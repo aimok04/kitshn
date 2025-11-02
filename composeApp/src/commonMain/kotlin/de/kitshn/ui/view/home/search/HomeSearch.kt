eb417602d66b354aa1497b7f2fdc10a90bcc3ae4
aimok04 <aimok04@mailbox.org>
2025-11-02 19:20:59 +0100

    fix(commonMain/home): replaced ExpandedDockedSearchbar with ExpandedFullScreenSearchbar again but its hidden when searchBarState != expanded (resolved #289)

--- composeApp/src/commonMain/kotlin/de/kitshn/ui/view/home/search/HomeSearch.kt
+++ composeApp/src/commonMain/kotlin/de/kitshn/ui/view/home/search/HomeSearch.kt
@@ -12,7 +12,7 @@ import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.AppBarWithSearch
-import androidx.compose.material3.ExpandedDockedSearchBar
+import androidx.compose.material3.ExpandedFullScreenSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -156,7 +156,7 @@ fun HomeSearchTopBar(
         scrollBehavior = scrollBehavior
     )
 
-    ExpandedDockedSearchBar(
+    if(searchBarState.currentValue == SearchBarValue.Expanded) ExpandedFullScreenSearchBar(
         state = searchBarState,
         inputField = inputField
     ) {
