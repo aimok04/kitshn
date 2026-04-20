39100dfcd440b589c2ab1bfd32339133a0a7add0
Jonas Schneider <jonas.max.schneider@gmail.com>
2026-04-20 20:44:49 +0200

    fix: use kmp library

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.android.kmp.library).apply(false)
     alias(libs.plugins.android.builtInKotlin).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
     alias(libs.plugins.buildConfig).apply(false)
