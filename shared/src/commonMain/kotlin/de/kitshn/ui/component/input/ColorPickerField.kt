060079d4f4a72797523e3cedf56dd007e23f20c2
aimok04 <aimok04@mailbox.org>
2026-05-01 22:18:06 +0200

    fix(ui): add showColorPickerDialog = false after hiding bottom sheet in ColorPickerField

--- shared/src/commonMain/kotlin/de/kitshn/ui/component/input/ColorPickerField.kt
+++ shared/src/commonMain/kotlin/de/kitshn/ui/component/input/ColorPickerField.kt
@@ -200,6 +200,7 @@ fun BaseColorPickerField(
                                 onValueChange(color)
                                 scope.launch {
                                     sheetState.hide()
+                                    showColorPickerDialog = false
                                 }
                             }
                     ) {
