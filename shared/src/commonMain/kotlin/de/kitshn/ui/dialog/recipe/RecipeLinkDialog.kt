f1a234fa126fc4f6cd2263c226a928442feb4410
aimok04 <aimok04@mailbox.org>
2026-05-10 16:37:02 +0200

    fix(ui/dialog): remove spacing between back and fullscreen buttons in RecipeLinkDialog.kt

--- shared/src/commonMain/kotlin/de/kitshn/ui/dialog/recipe/RecipeLinkDialog.kt
+++ shared/src/commonMain/kotlin/de/kitshn/ui/dialog/recipe/RecipeLinkDialog.kt
@@ -1,8 +1,6 @@
 package de.kitshn.ui.dialog.recipe
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Fullscreen
 import androidx.compose.material.icons.rounded.FullscreenExit
@@ -18,8 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import de.kitshn.api.tandoor.model.recipe.TandoorRecipeOverview
 import de.kitshn.ui.component.buttons.BackButton
 import de.kitshn.ui.component.buttons.BackButtonType
@@ -125,7 +121,6 @@ fun RecipeLinkDialog(
                     )
 
                     if (state.maximized.value || !isFullscreen) {
-                        Spacer(Modifier.width(8.dp))
                         FilledIconButton(
                             onClick = { state.maximized.value = !state.maximized.value },
                             colors = IconButtonDefaults.filledIconButtonColors(
